753ac8b61b7f5f89339db38b1fb25d2615797a16
Jupiter <varrat.asanka@gmail.com>
2023-07-25 16:24:35 +0200

    pipeline tentative

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask , request, render_template
 from model import predict
+import git
 
 #pyhtonanywhere templates folder path
 var_template_folder = '/home/Alwis/openclasroom-projet-7/templates/'
@@ -14,6 +15,18 @@ def home():
         label = predict(user_input)
         return render_template('form.html', prediction_text = label)
     return render_template('form.html')
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/RoiJupiter/openclasroom-projet-7')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
 
 if __name__ == '__main__':
     app.run()
