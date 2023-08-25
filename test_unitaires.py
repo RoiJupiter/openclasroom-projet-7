5e866c8a184da2271048429371a9d23fc066f487
Jupiter <varrat.asanka@gmail.com>
2023-08-25 14:56:07 +0200

    deploiement avec les test unitaire

---
+++ test_unitaires.py
@@ -0,0 +1,20 @@
+from model import decode_sentiment
+from keras_preprocessing.sequence import pad_sequences
+
+# Test unitaire
+
+def test_predict():
+
+    # Cas de test avec score > 0.5
+    score = 0.8
+    expected_output = "POSITIVE"
+    assert decode_sentiment(score) == expected_output
+
+    print("cas de test avec score > 0.5 test unitaire passé")
+
+    # Cas de test avec score <= 0.5
+    score = 0.3
+    expected_output = "NEGATIVE"
+    assert decode_sentiment(score) <= expected_output
+
+    print("Cas de test avec score <= 0.5 test unitaire passé")
